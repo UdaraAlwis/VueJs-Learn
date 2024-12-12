27e914e1309e744cf40347cda9fa8574b346ff29
Alwis <udara.robotics@gmail.com>
2024-12-12 16:37:18 +0100

    slots challenge

---
+++ vue-course-homework/Day 2/reusable-modal-component-1/src/AppModal.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { XMarkIcon } from "@heroicons/vue/20/solid";
+// Start your component logic here
+</script>
+
+<template>
+  <!--  Start your component template here -->
+  <div class="modal-body">
+    <div class="modal-header">
+      <h3>{{ title }}</h3>
+      <button @click="closeModal">
+        <XMarkIcon />
+      </button>
+    </div>
+    <slot></slot>
+  </div>
+</template>
